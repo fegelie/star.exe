b066be3a190a3b5ab5a120f9d3ef80bec5407ff8
Fegelie <51818687+fegelie@users.noreply.github.com>
2019-10-21 12:41:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,12 +23,14 @@ def callback():
     user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
     userText = decoded['queryResult']['intent']['displayName']
     #sendText(user,userText)
-    if (userText == 'โครงสร้างโลกแบ่งออกเป็นกี่ประเภทและมีอะไรบ้าง') :
-        sendText(user,'แบ่งอออกเป็น 3 ประเภท ประกออบด้วย 1.เปลือกโลก(Crust) 2.เนื้อโลก (Mantle) 3.แก่นโลก(Core)นะครับ')
-    elif (userText == 'โลกมีอายุประมาณกี่ปีแล้ว') :
-        sendText(user,'โลกมีอายุประมาณ4,600ล้านปีแล้วนะครับ')  
-    return '',200
-def sendText(user, text):
+    if (userText == 'How there you think about the vatherland') :
+        sendText(user,'germany startup to yhe great war.')
+    elif (userText == 'what is you think about him') :
+        sendText(user,'Who?')  
+    elif (userText == 'The kaiser') :
+        sendText(user,'The great man.')  
+      return '',200
+ def sendText(user, text):
   LINE_API = 'https://api.line.me/v2/bot/message/reply'
   headers = {
     'Content-Type': 'application/json; charset=UTF-8',
